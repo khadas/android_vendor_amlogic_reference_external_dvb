97e965e983462ddf2bfdee58385e079e32437ded
Zhiqiang Han <zhiqiang.han@amlogic.com>
2023-06-09 02:46:40 +0000

    typo: fix ident style [1/1]
    PD#SWPL-121376
    
    Problem:
    mixed ident style
    
    Solution:
    match the ident style
    
    Verify:
    PB
    
    Change-Id: If5e515090e7a47face944c0959f250571cb213bf
    Signed-off-by: Zhiqiang Han <zhiqiang.han@amlogic.com>

--- am_adp/am_open_lib/libdvbsi/descriptors/dr_7f.c
+++ am_adp/am_open_lib/libdvbsi/descriptors/dr_7f.c
@@ -48,7 +48,7 @@
  *****************************************************************************/
 int dvbpsi_Decode_Exten_Sup_Audio_Dr(dvbpsi_EXTENSION_dr_t * p_decoded, uint8_t * p_data, uint8_t i_length)
 {
-    AM_DEBUG(1, "dr_7f dvbpsi_Decode_Exten_Sup_Audio_Dr ");
+  AM_DEBUG(1, "dr_7f dvbpsi_Decode_Exten_Sup_Audio_Dr ");
   /*1 5 1 1 8bit mix:1 edit:5 re:1 lang:1*/
   p_decoded->exten_t.sup_audio.mix_type = (p_data[1]&0x80)>>7; /*1000 0000*/
   p_decoded->exten_t.sup_audio.editorial_classification = (p_data[1]&0x7C)>>2; /*0111 1100*/
@@ -69,12 +69,12 @@ int dvbpsi_Decode_Exten_Sup_Audio_Dr(dvbpsi_EXTENSION_dr_t * p_decoded, uint8_t 
  *****************************************************************************/
 int dvbpsi_Decode_Exten_AC4_Audio_Dr(dvbpsi_EXTENSION_dr_t * p_decoded, uint8_t * p_data, uint8_t i_length)
 {
-	UNUSED(i_length);
-	AM_DEBUG(1, "dr_7f dvbpsi_Decode_Exten_AC4_Audio_Dr ");
-	/*1 5 1 1 8bit mix:1 edit:5 re:1 lang:1*/
-	p_decoded->exten_t.ac4_audio.ac4_config_flag = p_data[1]; /*1000 0000*/
-	p_decoded->exten_t.ac4_audio.ac4_toc_flag = p_data[1]; /*0111 1100*/
-	return 0;
+  UNUSED(i_length);
+  AM_DEBUG(1, "dr_7f dvbpsi_Decode_Exten_AC4_Audio_Dr ");
+  /*1 5 1 1 8bit mix:1 edit:5 re:1 lang:1*/
+  p_decoded->exten_t.ac4_audio.ac4_config_flag = p_data[1]; /*1000 0000*/
+  p_decoded->exten_t.ac4_audio.ac4_toc_flag = p_data[1]; /*0111 1100*/
+  return 0;
 }
 
 /*****************************************************************************
@@ -82,83 +82,83 @@ int dvbpsi_Decode_Exten_AC4_Audio_Dr(dvbpsi_EXTENSION_dr_t * p_decoded, uint8_t 
  *****************************************************************************/
 int dvbpsi_Decode_Exten_Audio_Preselection_Dr(dvbpsi_EXTENSION_dr_t * p_decoded, uint8_t * p_data, uint8_t i_length)
 {
-	UNUSED(i_length);
-	AM_DEBUG(1, "dr_7f dvbpsi_Decode_Exten_Audio_Preselection_Dr ");
-	dvbpsi_EXTENSION_audio_preselection_t* ap = &p_decoded->exten_t.audio_preselection;
-	ap->num_preselections = (p_data[1]&0xF8)>>3; /*1111 1000*/
-	ap->reserved_zero_future_use = (p_data[1]&0x07); /*0000 0111*/
-	if (ap->num_preselections <= 0)
-	{
-		return 0;
-	}
-	uint8_t* data = p_data+2;
-	// TV-64939 mentions the use case of multi_stream_info_present.
-	// It requires to skip the audio track which multi_stream_info_present is TRUE.
-	// num_skipped_presel here is used to count number of preselections to be skipped
-	// along this line.
-	int num_skipped_presel = 0;
-	for (int i=0;i<ap->num_preselections;i++)
-	{
-		// Considering the skipped preselection item, here is 'i-num_skipped_presel'
-		dvbpsi_EXTENSION_preselection_t* ps_i = &ap->preselections[i-num_skipped_presel];
-		int extra=0;
-		ps_i->preselection_id = (data[0]&0xF8)>>3;
-		ps_i->audio_rendering_indication = (data[0]&0x07);
-		ps_i->audio_description = (data[1]&0x80)>>7;
-		ps_i->spoken_subtitle = (data[1]&0x40)>>6;
-		ps_i->dialogue_enhancement = (data[1]&0x20)>>5;
-		ps_i->interactivity_enabled = (data[1]&0x10)>>4;
-		ps_i->language_code_present = (data[1]&0x08)>>3;
-		ps_i->text_label_present = (data[1]&0x04)>>2;
-		ps_i->multi_stream_info_present = (data[1]&0x02)>>1;
-		ps_i->future_extension = (data[1]&0x01);
-		if (ps_i->language_code_present)
-		{
-			strncpy((char *)ps_i->iso_639_language_code,(char *)data+2,3);
-			extra+=3;
-		}
-		ps_i->message_id=0;
-		if (ps_i->text_label_present)
-		{
-			ps_i->message_id = data[2+extra];
-			extra+=1;
-		}
-		if (ps_i->multi_stream_info_present)
-		{
-			int num_aux_components = data[2+extra]>>5;
-			extra+=num_aux_components+1;
-			num_skipped_presel++;
-		}
-		if (ps_i->future_extension)
-		{
-			int future_extension_length = data[2+extra];
-			extra+=future_extension_length+1;
-		}
-		AM_DEBUG(1, "dr_7f data:%p, preselection id:%d, lang:%c%c%c, msg_id:%d",
-				data,(int)(ps_i->preselection_id),
-				ps_i->iso_639_language_code[0], ps_i->iso_639_language_code[1],
-				ps_i->iso_639_language_code[2], (int)(ps_i->message_id));
-		data += (2+extra);
-	}
-	// num_preselections needs to be revised
-	ap->num_preselections -= num_skipped_presel;
-	return 0;
+  UNUSED(i_length);
+  AM_DEBUG(1, "dr_7f dvbpsi_Decode_Exten_Audio_Preselection_Dr ");
+  dvbpsi_EXTENSION_audio_preselection_t* ap = &p_decoded->exten_t.audio_preselection;
+  ap->num_preselections = (p_data[1]&0xF8)>>3; /*1111 1000*/
+  ap->reserved_zero_future_use = (p_data[1]&0x07); /*0000 0111*/
+  if (ap->num_preselections <= 0)
+  {
+    return 0;
+  }
+  uint8_t* data = p_data+2;
+  // TV-64939 mentions the use case of multi_stream_info_present.
+  // It requires to skip the audio track which multi_stream_info_present is TRUE.
+  // num_skipped_presel here is used to count number of preselections to be skipped
+  // along this line.
+  int num_skipped_presel = 0;
+  for (int i=0;i<ap->num_preselections;i++)
+  {
+    // Considering the skipped preselection item, here is 'i-num_skipped_presel'
+    dvbpsi_EXTENSION_preselection_t* ps_i = &ap->preselections[i-num_skipped_presel];
+    int extra=0;
+    ps_i->preselection_id = (data[0]&0xF8)>>3;
+    ps_i->audio_rendering_indication = (data[0]&0x07);
+    ps_i->audio_description = (data[1]&0x80)>>7;
+    ps_i->spoken_subtitle = (data[1]&0x40)>>6;
+    ps_i->dialogue_enhancement = (data[1]&0x20)>>5;
+    ps_i->interactivity_enabled = (data[1]&0x10)>>4;
+    ps_i->language_code_present = (data[1]&0x08)>>3;
+    ps_i->text_label_present = (data[1]&0x04)>>2;
+    ps_i->multi_stream_info_present = (data[1]&0x02)>>1;
+    ps_i->future_extension = (data[1]&0x01);
+    if (ps_i->language_code_present)
+    {
+      strncpy((char *)ps_i->iso_639_language_code,(char *)data+2,3);
+      extra+=3;
+    }
+    ps_i->message_id=0;
+    if (ps_i->text_label_present)
+    {
+      ps_i->message_id = data[2+extra];
+      extra+=1;
+    }
+    if (ps_i->multi_stream_info_present)
+    {
+      int num_aux_components = data[2+extra]>>5;
+      extra+=num_aux_components+1;
+      num_skipped_presel++;
+    }
+    if (ps_i->future_extension)
+    {
+      int future_extension_length = data[2+extra];
+      extra+=future_extension_length+1;
+    }
+    AM_DEBUG(1, "dr_7f data:%p, preselection id:%d, lang:%c%c%c, msg_id:%d",
+      data,(int)(ps_i->preselection_id),
+      ps_i->iso_639_language_code[0], ps_i->iso_639_language_code[1],
+      ps_i->iso_639_language_code[2], (int)(ps_i->message_id));
+    data += (2+extra);
+  }
+  // num_preselections needs to be revised
+  ap->num_preselections -= num_skipped_presel;
+  return 0;
 }
 
 int dvbpsi_Decode_Exten_Message_Dr(dvbpsi_EXTENSION_dr_t * p_decoded, uint8_t * p_data, uint8_t i_length)
 {
-	AM_DEBUG(1, "dr_7f %s", __func__);
-	dvbpsi_EXTENSION_message_t* ap = &p_decoded->exten_t.message;
-	ap->message_id = p_data[1];
-	strncpy((char *)ap->iso_639_language_code,(char *)p_data+2,3);
-	int text_len = i_length-5;
-	memset(ap->text,0,sizeof(ap->text));
-	strncpy((char *)ap->text,(char *)p_data+5,text_len);
-	uint8_t* lang = ap->iso_639_language_code;
-	AM_DEBUG(1, "dr_7f %s, msg_id:%d, lang:%c%c%c, text:%s.", __func__,
-			ap->message_id,lang[0],lang[1],lang[2],ap->text);
+  AM_DEBUG(1, "dr_7f %s", __func__);
+  dvbpsi_EXTENSION_message_t* ap = &p_decoded->exten_t.message;
+  ap->message_id = p_data[1];
+  strncpy((char *)ap->iso_639_language_code,(char *)p_data+2,3);
+  int text_len = i_length-5;
+  memset(ap->text,0,sizeof(ap->text));
+  strncpy((char *)ap->text,(char *)p_data+5,text_len);
+  uint8_t* lang = ap->iso_639_language_code;
+  AM_DEBUG(1, "dr_7f %s, msg_id:%d, lang:%c%c%c, text:%s.", __func__,
+    ap->message_id,lang[0],lang[1],lang[2],ap->text);
 
-	return 0;
+  return 0;
 }
 
 /*****************************************************************************
@@ -181,8 +181,7 @@ dvbpsi_EXTENSION_dr_t * dvbpsi_DecodeEXTENSIONDr(dvbpsi_descriptor_t * p_descrip
     return p_descriptor->p_decoded;
 
   /* Allocate memory */
-  p_decoded =
-        (dvbpsi_EXTENSION_dr_t *)malloc(sizeof(dvbpsi_EXTENSION_dr_t));
+  p_decoded = (dvbpsi_EXTENSION_dr_t *)malloc(sizeof(dvbpsi_EXTENSION_dr_t));
   if (!p_decoded)
   {
     DVBPSI_ERROR("dr_7f decoder", "out of memory");
@@ -263,8 +262,7 @@ dvbpsi_EXTENSION_dr_t * dvbpsi_DecodeEXTENSIONDr(dvbpsi_descriptor_t * p_descrip
       break;
     case AM_SI_EXTEN_DESCR_AUDIO_PRESELECTION:
       AM_DEBUG(1, "dr_7f exten tag AM_SI_EXTEN_DESCR_AUDIO_PRESELECTION ");
-      dvbpsi_Decode_Exten_Audio_Preselection_Dr(
-          p_decoded, p_descriptor->p_data, p_descriptor->i_length);
+      dvbpsi_Decode_Exten_Audio_Preselection_Dr(p_decoded, p_descriptor->p_data, p_descriptor->i_length);
       break;
     case AM_SI_EXTEN_DESCR_OTHER:
       AM_DEBUG(1, "dr_7f exten tag AM_SI_EXTEN_DESCR_OTHER ");
